1f7ee0f16284edc0da87cfef7a18a447fb40e0da
Tom Elmore <github@atinian.co.uk>
2021-01-02 14:25:53 +0000

    chore: Adds deps for test project & naive example test

---
+++ ClearBank.DeveloperTest.Tests/Services/PaymentsServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using ClearBank.DeveloperTest.Services;
+using ClearBank.DeveloperTest.Types;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClearBank.DeveloperTest.Tests.Services
+{
+    [TestFixture]
+    public class PaymentsServiceTests
+    {
+        [Test]
+        public void MakePayment_ReturnsNotNull()
+        {
+            // Arrange
+            var service = new PaymentService();
+
+            // Act
+            MakePaymentResult result = service.MakePayment(new MakePaymentRequest());
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
