d3e5e1d5077d646185a563ff938535c0b2acca40
Tom Elmore <github@atinian.co.uk>
2021-01-03 09:48:54 +0000

    feat: Adds helper factory to build the service from its collaborators

---
+++ ClearBank.DeveloperTest.Tests/Services/PaymentSericeFactoryTests.cs
@@ -0,0 +1,22 @@
+﻿using ClearBank.DeveloperTest.Services;
+using NUnit.Framework;
+
+namespace ClearBank.DeveloperTest.Tests.Services
+{
+    [TestFixture]
+    public class PaymentSericeFactoryTests
+    {
+        [Test]
+        public void BuildsService()
+        {
+            // Arrange
+            var factory = new PaymentServiceFactory();
+
+            // Act
+            var service = factory.BuildPaymentService();
+
+            // Assert
+            Assert.IsNotNull(service);
+        }
+    }
+}
---
+++ ClearBank.DeveloperTest/Services/PaymentServiceFactory.cs
@@ -0,0 +1,20 @@
+﻿using ClearBank.DeveloperTest.Configuration;
+using ClearBank.DeveloperTest.Data;
+using ClearBank.DeveloperTest.Validation;
+
+namespace ClearBank.DeveloperTest.Services
+{
+    public class PaymentServiceFactory : IPaymentServiceFactory
+    {
+        public IPaymentService BuildPaymentService()
+        {
+            var configurationProvider = new ConfigurationProvider();
+
+            var accountDataStoreFactory = new AccountDataStoreFactory(configurationProvider);
+
+            var validatorFactory = new ValidatorFactory();
+
+            return new PaymentService(accountDataStoreFactory, validatorFactory);
+        }
+    }
+}
